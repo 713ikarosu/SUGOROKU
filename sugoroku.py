1e4ebb1888591ca545f97811a7e40fbde8e65146
soudahirokatsu <hirokatsu@soudahirokatsu-no-MacBook-Air.local>
2016-12-05 17:38:25 +0900

    no message

--- sugoroku.py
+++ sugoroku.py
@@ -1,16 +1,46 @@
 import random
 
-masu = 30
+masu = 0
 saikoro = 6
 rest = masu
 
+
 print ('ゲームスタート')
-while rest > 0:
+def main():
+    global rest
+    while rest > 0:
+        SaikoroSystem()
+        RestInput()
+        Goal()
+
+def masuDecision():
+    global masu
+    global rest
+    masu = int(input('マス数を入力してください(20~39)：'))
+    rest = masu
+    while masu < 20 or masu > 39:
+        masu = int(input('範囲内で入力してください(20~39)：'))
+        rest = masu
+
+def Goal():
+    global rest
+    if rest == 0:
+        print ('ゴール！')
+    elif rest < 0:
+        rest *= -1
+        print ('残り'+ str(rest) + 'マスです')
+
+def SaikoroSystem():
+    global rest
     value = random.randint(1,saikoro)
     print ('サイコロの目：' + str(value))
     rest -= value
+
+
+def RestInput():
     if rest > 0:
         print ('残り' + str(rest) + 'マスです')
 
-if rest <= 0:
-    print ('ゴール！')
+
+masuDecision()
+main()
